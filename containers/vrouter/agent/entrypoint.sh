5e824e5b236c3f5d2ce7af27c0a8fa9594242a53
Ivan Timofeev <ivanv.timofeev@gmail.com>
2020-09-08 10:31:40 +0300

    Fix vrouter agent signals
    Change-Id: Ib104c29ce20915c0c575b0072280441dd32aee54

--- containers/vrouter/agent/entrypoint.sh
+++ containers/vrouter/agent/entrypoint.sh
@@ -6,8 +6,20 @@ source /actions.sh
 
 echo "INFO: agent started in $AGENT_MODE mode"
 
-set_traps
+ # Clean up files and vhost0, when SIGQUIT signal by clean-up.sh
+trap 'trap_vrouter_agent_quit' SIGQUIT
+
+# Terminate process only.
+# When a container/pod restarts it sends TERM and KILL signal.
+# Every time container restarts we dont want to reset data plane
+trap 'trap_vrouter_agent_term' SIGTERM SIGINT
+
+# Send SIGHUP signal to child process
+trap 'trap_vrouter_agent_hub' SIGHUP
 
 vhost0_init $@
 
-wait $(run_agent $@)
+echo "DEBUG: Just before run agent"
+vrouter_agent_process=$(run_agent $@)
+echo "DEBUG: vrouter agent process outside fuction = $vrouter_agent_process"
+wait $vrouter_agent_process
