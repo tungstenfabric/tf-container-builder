0395dca222d7f1fe7e6fcd1f2b2432cda71baece
Kirill Egorov <kegorov@progmaticlab.com>
2020-09-13 20:25:43 +0300

    Third part of tf-topology renaming
    Depends-On: I67116319d3c6aa981d81c56d9c592124a85b4cd7
    Depends-On:Ie9e46dc1563e3c493978caefd72d8f611eef5b81
    
    partial-jira-bug: CEM-17923
    Change-Id: I1970c1ec1294b3698356976ca025eade6674a488

--- containers/analytics/snmp/topology/entrypoint.sh
+++ containers/analytics/snmp/topology/entrypoint.sh
@@ -9,13 +9,13 @@ rabbitmq_server_list=$(echo $RABBITMQ_SERVERS | sed 's/,/ /g')
 config_db_server_list=$(echo $CONFIGDB_SERVERS | sed 's/,/ /g')
 
 mkdir -p /etc/contrail
-cat > /etc/contrail/contrail-topology.conf << EOM
+cat > /etc/contrail/tf-topology.conf << EOM
 [DEFAULTS]
 host_ip=${host_ip}
 scan_frequency=${TOPOLOGY_SCAN_FREQUENCY:-600}
 http_server_port=${TOPOLOGY_INTROSPECT_LISTEN_PORT:-$TOPOLOGY_INTROSPECT_PORT}
 http_server_ip=$(get_introspect_listen_ip_for_node ANALYTICS_SNMP)
-log_file=$CONTAINER_LOG_DIR/contrail-topology.log
+log_file=$CONTAINER_LOG_DIR/tf-topology.log
 log_level=$LOG_LEVEL
 log_local=$LOG_LOCAL
 analytics_api=$ANALYTICS_SERVERS
@@ -40,7 +40,7 @@ $sandesh_client_config
 $collector_stats_config
 EOM
 
-add_ini_params_from_env TOPOLOGY /etc/contrail/contrail-topology.conf
+add_ini_params_from_env TOPOLOGY /etc/contrail/tf-topology.conf
 
 set_third_party_auth_config
 set_vnc_api_lib_ini
