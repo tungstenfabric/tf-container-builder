97da46f63811bb02442fcbc547cc3e6958f9a511
Aditi Kumari <aditik@juniper.net>
2020-09-23 12:16:27 -0700

    Generate config files for workers in entrypoint.sh
    Closes-Jira-Bug: CEM-19232
    
    For deployment of api server with uwsgi, generate seperate config
    files for workers in entrypoint.sh. The workers will be spawned
    using their respective config files based on worker id.
    
    Signed-off-by: Aditi Kumari <aditik@juniper.net>
    Change-Id: I3ddb5597edc24cb8f7f85e89c9d5a36a056d4414
    Depends-On: I245d7cced2eb6b54b47e10cda5891a29ba6f5a30
    Depends-On: I95e085b7d6fdb3e6e630772fe453055599410b06

--- containers/controller/config/api/entrypoint.sh
+++ containers/controller/config/api/entrypoint.sh
@@ -32,11 +32,22 @@ else
 fi
 
 mkdir -p /etc/contrail
-cat > /etc/contrail/contrail-api.conf << EOM
+
+ADMIN_PORT=8095
+for ((index=0; index<$CONFIG_API_WORKER_COUNT; ++index)) ; do
+  if [[ "$index" = 0 ]] ; then
+    http_server_port=${CONFIG_API_INTROSPECT_PORT}
+    admin_port=${ADMIN_PORT}
+  else
+    http_server_port=$((10000 + CONFIG_API_INTROSPECT_PORT + index - 1))
+    admin_port=$((20000 + ADMIN_PORT + index - 1))
+  fi
+
+cat > /etc/contrail/contrail-api-$index.conf << EOM
 [DEFAULTS]
 listen_ip_addr=${host_ip}
 listen_port=$CONFIG_API_PORT
-http_server_port=${CONFIG_API_INTROSPECT_PORT}
+http_server_port=${http_server_port}
 http_server_ip=$(get_introspect_listen_ip_for_node CONFIG)
 log_file=$CONTAINER_LOG_DIR/contrail-api.log
 log_level=$LOG_LEVEL
@@ -51,7 +62,8 @@ cassandra_use_ssl=${CASSANDRA_SSL_ENABLE,,}
 cassandra_ca_certs=$CASSANDRA_SSL_CA_CERTFILE
 zk_server_ip=$ZOOKEEPER_SERVERS
 
-$config_api_certs_config
+admin_port=${admin_port}
+worker_id=$index
 
 rabbit_server=$RABBITMQ_SERVERS
 $rabbit_config
@@ -65,8 +77,32 @@ $collector_stats_config
 
 $neutron_section
 EOM
+done
 
-add_ini_params_from_env API /etc/contrail/contrail-api.conf
+if is_enabled ${CONFIG_API_SSL_ENABLE} ; then
+  uwsgi="https-socket = ${host_ip}:$CONFIG_API_PORT,${CONFIG_API_SERVER_CERTFILE},${CONFIG_API_SERVER_KEYFILE}"
+else
+  uwsgi_socket="protocol = http
+  socket = ${host_ip}:$CONFIG_API_PORT"
+fi
+cat> /etc/contrail/contrail-api-uwsgi.ini <<EOM
+[uwsgi]
+strict
+master
+single-interpreter
+vacuum
+need-app
+workers = ${CONFIG_API_WORKER_COUNT}
+gevent = ${CONFIG_API_MAX_REQUESTS}
+lazy-apps
+
+$uwsgi_socket
+module = vnc_cfg_api_server.uwsgi_api_server:get_apiserver()
+so-keepalive
+reuse-port
+uid = $CONTRAIL_UID
+gid = $CONTRAIL_GID
+EOM
 
 set_third_party_auth_config
 set_vnc_api_lib_ini
