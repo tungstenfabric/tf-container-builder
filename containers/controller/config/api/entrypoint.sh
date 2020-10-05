8ddfe38dcf901f4a60da4452c211e11c337958aa
alexey-mr <alexey.morlang@gmail.com>
2020-10-05 13:15:51 +0300

    Fixed ssl case and remove code duplication in uwsgi support
    - fixed log files for workers
    - fixed uwsgi path as rpm and pip use different paths
    /usr/sbin/ and /usr/bin
    
    partial-jira-bug: CEM-19232
    Change-Id: I4a67c2e2b0b2b7d6d9e0309602bd38be2b67faa9
    (cherry picked from commit 2af814581f8349eb7c5ecbd268154c2fe726b0a0)

--- containers/controller/config/api/entrypoint.sh
+++ containers/controller/config/api/entrypoint.sh
@@ -11,15 +11,19 @@ if ! is_enabled ${CONFIG_API_LISTEN_ALL}; then
 fi
 
 cassandra_server_list=$(echo $CONFIGDB_SERVERS | sed 's/,/ /g')
+config_api_certs_config=''
+uwsgi_socket="protocol = http\nsocket = ${host_ip}:$CONFIG_API_PORT"
 if is_enabled ${CONFIG_API_SSL_ENABLE} ; then
-  read -r -d '' config_api_certs_config << EOM || true
+  if (( CONFIG_API_WORKER_COUNT == 1 )) ; then
+    read -r -d '' config_api_certs_config << EOM || true
 config_api_ssl_enable=${CONFIG_API_SSL_ENABLE}
 config_api_ssl_certfile=${CONFIG_API_SERVER_CERTFILE}
 config_api_ssl_keyfile=${CONFIG_API_SERVER_KEYFILE}
 config_api_ssl_ca_cert=${CONFIG_API_SERVER_CA_CERTFILE}
 EOM
-else
-  config_api_certs_config=''
+  else
+    uwsgi_socket="https-socket = ${host_ip}:$CONFIG_API_PORT,${CONFIG_API_SERVER_CERTFILE},${CONFIG_API_SERVER_KEYFILE}"
+  fi
 fi
 
 if is_enabled ${FWAAS_ENABLE} ; then
@@ -33,14 +37,16 @@ fi
 
 mkdir -p /etc/contrail
 
-if [[ "$CONFIG_API_WORKER_COUNT" == 1 ]] ; then
-cat > /etc/contrail/contrail-api.conf << EOM
+admin_port=$CONFIG_API_ADMIN_PORT
+http_server_port=$CONFIG_API_INTROSPECT_PORT
+for (( index=0; index < CONFIG_API_WORKER_COUNT; ++index )) ; do
+  cat > /etc/contrail/contrail-api-$index.conf << EOM
 [DEFAULTS]
 listen_ip_addr=${host_ip}
 listen_port=$CONFIG_API_PORT
-http_server_port=${CONFIG_API_INTROSPECT_PORT}
+http_server_port=${http_server_port}
 http_server_ip=$(get_introspect_listen_ip_for_node CONFIG)
-log_file=$CONTAINER_LOG_DIR/contrail-api.log
+log_file=$CONTAINER_LOG_DIR/contrail-api-${index}.log
 log_level=$LOG_LEVEL
 log_local=$LOG_LOCAL
 list_optimization_enabled=${CONFIG_API_LIST_OPTIMIZATION_ENABLED:-True}
@@ -55,53 +61,8 @@ zk_server_ip=$ZOOKEEPER_SERVERS
 
 $config_api_certs_config
 
-rabbit_server=$RABBITMQ_SERVERS
-$rabbit_config
-$kombu_ssl_config
-
-collectors=$COLLECTOR_SERVERS
-
-$sandesh_client_config
-
-$collector_stats_config
-
-$neutron_section
-EOM
-
-add_ini_params_from_env API /etc/contrail/contrail-api.conf
-
-else
-ADMIN_PORT=8095
-for ((index=0; index<$CONFIG_API_WORKER_COUNT; ++index)) ; do
-  if [[ "$index" == 0 ]] ; then
-    http_server_port=${CONFIG_API_INTROSPECT_PORT}
-    admin_port=${ADMIN_PORT}
-  else
-    http_server_port=$((10000 + CONFIG_API_INTROSPECT_PORT + index - 1))
-    admin_port=$((20000 + ADMIN_PORT + index - 1))
-  fi
-
-cat > /etc/contrail/contrail-api-$index.conf << EOM
-[DEFAULTS]
-listen_ip_addr=${host_ip}
-listen_port=$CONFIG_API_PORT
-http_server_port=${http_server_port}
-http_server_ip=$(get_introspect_listen_ip_for_node CONFIG)
-log_file=$CONTAINER_LOG_DIR/contrail-api.log
-log_level=$LOG_LEVEL
-log_local=$LOG_LOCAL
-list_optimization_enabled=${CONFIG_API_LIST_OPTIMIZATION_ENABLED:-True}
-auth=$AUTH_MODE
-aaa_mode=$AAA_MODE
-cloud_admin_role=$CLOUD_ADMIN_ROLE
-global_read_only_role=$GLOBAL_READ_ONLY_ROLE
-cassandra_server_list=$cassandra_server_list
-cassandra_use_ssl=${CASSANDRA_SSL_ENABLE,,}
-cassandra_ca_certs=$CASSANDRA_SSL_CA_CERTFILE
-zk_server_ip=$ZOOKEEPER_SERVERS
-
 admin_port=${admin_port}
-worker_id=$index
+worker_id=${index}
 
 rabbit_server=$RABBITMQ_SERVERS
 $rabbit_config
@@ -115,20 +76,24 @@ $collector_stats_config
 
 $neutron_section
 EOM
+
+  add_ini_params_from_env API /etc/contrail/contrail-api-$index.conf
+
+  http_server_port=$(( 10000 + CONFIG_API_INTROSPECT_PORT + index ))
+  admin_port=$(( 20000 + CONFIG_API_ADMIN_PORT + index ))
+
 done
 
-if is_enabled ${CONFIG_API_SSL_ENABLE} ; then
-  uwsgi_socket="https-socket = ${host_ip}:$CONFIG_API_PORT,${CONFIG_API_SERVER_CERTFILE},${CONFIG_API_SERVER_KEYFILE}"
-else
-  uwsgi_socket="protocol = http\nsocket = ${host_ip}:$CONFIG_API_PORT"
-fi
-cat> /etc/contrail/contrail-api-uwsgi.ini <<EOM
+if (( CONFIG_API_WORKER_COUNT > 1 )) ; then
+  service_cmd="$(which uwsgi) /etc/contrail/contrail-api-uwsgi.ini"
+  cat > /etc/contrail/contrail-api-uwsgi.ini <<EOM
 [uwsgi]
 strict
 master
 single-interpreter
 vacuum
 need-app
+plugins = python, gevent
 workers = ${CONFIG_API_WORKER_COUNT}
 gevent = ${CONFIG_API_MAX_REQUESTS}
 lazy-apps
@@ -137,9 +102,9 @@ $(printf '%b\n' "$uwsgi_socket")
 module = vnc_cfg_api_server.uwsgi_api_server:get_apiserver()
 so-keepalive
 reuse-port
-uid = $CONTRAIL_UID
-gid = $CONTRAIL_GID
 EOM
+else
+  service_cmd="/usr/bin/contrail-api --conf_file /etc/contrail/contrail-api-0.conf --conf_file /etc/contrail/contrail-keystone-auth.conf --worker_id 0"
 fi
 
 set_third_party_auth_config
@@ -147,10 +112,4 @@ set_vnc_api_lib_ini
 
 upgrade_old_logs "contrail-api"
 
-if [[ "$CONFIG_API_WORKER_COUNT" == 1 ]] ; then
-  /usr/bin/contrail-api --conf_file /etc/contrail/contrail-api.conf --conf_file /etc/contrail/contrail-keystone-auth.conf --worker_id 0
-else
-  /usr/bin/uwsgi /etc/contrail/contrail-api-uwsgi.ini
-fi
-
-run_service "$@"
+run_service $service_cmd
