d417cafb6e9bc447f8280fdba2e5c934c322ff62
Kirill Egorov <kegorov@progmaticlab.com>
2020-10-09 03:11:50 +0300

    Rename snmp-collector Contrail->TF (Third part of renaming)
    jira-partial-bug: CEM-19634
    Change-Id: Ie19d30a3bb773ee036fa8db8261ae12f8d46373f

--- containers/analytics/snmp/collector/entrypoint.sh
+++ containers/analytics/snmp/collector/entrypoint.sh
@@ -10,14 +10,14 @@ rabbitmq_server_list=$(echo $RABBITMQ_SERVERS | sed 's/,/ /g')
 config_db_server_list=$(echo $CONFIGDB_SERVERS | sed 's/,/ /g')
 
 mkdir -p /etc/contrail
-cat > /etc/contrail/contrail-snmp-collector.conf << EOM
+cat > /etc/contrail/tf-snmp-collector.conf << EOM
 [DEFAULTS]
 host_ip=${host_ip}
 scan_frequency=${SNMPCOLLECTOR_SCAN_FREQUENCY:-600}
 fast_scan_frequency=${SNMPCOLLECTOR_FAST_SCAN_FREQUENCY:-60}
 http_server_ip=$(get_introspect_listen_ip_for_node ANALYTICS_SNMP)
 http_server_port=${SNMPCOLLECTOR_INTROSPECT_LISTEN_PORT:-$SNMPCOLLECTOR_INTROSPECT_PORT}
-log_file=$CONTAINER_LOG_DIR/contrail-snmp-collector.log
+log_file=$CONTAINER_LOG_DIR/tf-snmp-collector.log
 log_level=$LOG_LEVEL
 log_local=$LOG_LOCAL
 collectors=$COLLECTOR_SERVERS
@@ -41,7 +41,7 @@ $sandesh_client_config
 $collector_stats_config
 EOM
 
-add_ini_params_from_env SNMP_COLLECTOR /etc/contrail/contrail-snmp-collector.conf
+add_ini_params_from_env SNMP_COLLECTOR /etc/contrail/tf-snmp-collector.conf
 
 set_third_party_auth_config
 set_vnc_api_lib_ini
