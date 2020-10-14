1e486826da581a7c046dd85002cee7eeb60d7e21
Ivan Timofeev <ivanv.timofeev@gmail.com>
2020-10-14 11:30:21 +0300

    Add reload_agent_config function
    partial-jira-bug: CEM-19606
    Change-Id: I10cde009f33bc417dcf9298924ae8afbadc66c13

--- containers/vrouter/base/agent-functions.sh
+++ containers/vrouter/base/agent-functions.sh
@@ -18,8 +18,12 @@ function trap_vrouter_agent_term() {
     term_process $(cat /var/run/vrouter-agent.pid)
 }
 
-function trap_vrouter_agent_hub() {
-    send_sighup_child_process $(cat /var/run/vrouter-agent.pid)
+function trap_vrouter_agent_hup() {
+    # TODO when vrouter agent binary will support SIGHUP
+    # We can just run " kill -HUP $(cat /var/run/vrouter-agent.pid) "
+    # But now we have to restart agent process fir reload agent config file
+    term_process $(cat /var/run/vrouter-agent.pid)
+    touch /var/run/restart_agent
 }
 
 #Agents constants
