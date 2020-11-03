0547940e7d0205c367d9f263aa8f563e54d8b321
Arina Argentova <aargentova@progmaticlab.com>
2020-11-03 16:33:37 +0300

    [~]  Contrail Tools is now working when used in non-interactive node
    partial-jira-bug: CEM-20132
    Change-Id: Ibd561aa296a736ba37dd832e6f12813547c19aa1

--- containers/node-init/contrail-tools-init.sh
+++ containers/node-init/contrail-tools-init.sh
@@ -32,7 +32,13 @@ fi
 
 
 image=$(echo ${CONTRAIL_STATUS_IMAGE} | sed 's/contrail-status:/contrail-tools:/')
-tmp_suffix="-it --rm --pid host --net host --privileged ${image}"
+if [ -t 0 ]; then
+    # interactive mode
+    tmp_suffix="-it --rm --pid host --net host --privileged ${image}"
+else
+    # non-interactive
+    tmp_suffix+="-i --rm --pid host --net host --privileged ${image}"
+fi
 tmp_file=/host/usr/bin/contrail-tools.tmp.${RANDOM}
 cat > $tmp_file << EOM
 #!/bin/bash
